7edcc5cfb8ad9d297c22984129c32ef4fbfde1be
Jeremiah RET <jackiejazmine69@gmail.com>
2023-07-16 11:41:42 +0700

    fix Eslint and CatagoryList

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import CategoryList from '../components/home/categoryList'
+import CategoryList from '../components/home/CategoryList'
 export default function Home() {
     return (
       <div className={styles.container}>
